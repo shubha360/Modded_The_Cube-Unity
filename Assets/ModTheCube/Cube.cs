f209ebc2a29d2e56ead9ac5689955e48c654f543
Raquibul Islam <shubha360@outlook.com>
2021-12-29 15:44:50 +0600

    First Commit

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public Color[] colors = new Color[2];
+
+    private int currentNumber = 3;
+
+    public float translationSpeed;
+    public float rotationSpeed;
+
+    private int positionBoundX = 4;
+    private bool goingRight = true;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+        Material material = Renderer.material;
+
+        if (goingRight && transform.position.x > positionBoundX)
+        {
+            goingRight = false;
+            material.color = colors[getRandomNumber()];
+        }
+        if (!goingRight && transform.position.x < -positionBoundX)
+        {
+            goingRight = true;
+            material.color = colors[getRandomNumber()];
+        }
+
+        if (goingRight)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * translationSpeed);
+            transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed);
+        }
+        else
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * -translationSpeed);
+            transform.Rotate(Vector3.left * Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    int getRandomNumber()
+    {
+        int newNumber = 3;
+
+        while (newNumber == currentNumber)
+        {
+            newNumber = Random.Range(0, colors.Length);
+        }
+
+        currentNumber = newNumber;
+        return newNumber;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Modded_The_Cube-Unity
 Completed code of the challenge task from Create with Code - 1 of Junior Programmer Pathway.
+
+[PLAY HERE](https://play.unity.com/mg/other/webgl-builds-128819)
+
+![tumbnail](1.jpg)
